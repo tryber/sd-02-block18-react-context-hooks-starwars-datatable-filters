d2108d93518873c7707c3b121f47c16d4ad3e4af
douglas-he <doughrqe@gmail.com>
2020-04-07 02:00:05 -0300

    CC

--- src/components/NumericFilter.jsx
+++ src/components/NumericFilter.jsx
@@ -3,23 +3,18 @@ import StarWarsContext from '../context/StarWarsContext';
 
 const NumericFilter = () => {
   const [selectColumn, setSelectColumn] = useState(['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water']);
+  const comparisonArray = ['maior que', 'menor que', 'igual a'];
   const [column, setColumn] = useState('');
   const [comparison, setComparison] = useState('');
   const [value, setValue] = useState('');
   const { addFilter, filters, excludeFilter } = useContext(StarWarsContext);
   const onAddFilter = () => {
     setSelectColumn(selectColumn.filter((col) => col !== column));
-    addFilter(column, comparison, value);
-    setColumn('');
-    setComparison('');
-    setValue('');
+    addFilter(column, comparison, value); setColumn(''); setComparison(''); setValue('');
   };
-
   const onExcludeFilter = (param) => {
-    setSelectColumn([...selectColumn, param]);
-    excludeFilter(param);
+    setSelectColumn([...selectColumn, param]); excludeFilter(param);
   };
-
   return (
     <div>
       <div>
@@ -31,40 +26,27 @@ const NumericFilter = () => {
         {column && (
           <select onChange={({ target }) => setComparison(target.value)}>
             <option value=""> </option>
-            <option value="maior que">Maior que</option>
-            <option value="menor que">Menor que</option>
-            <option value="igual a">Igual a</option>
+            {comparisonArray.map((comp) => <option key={comp} value={comp}>{comp}</option>)}
           </select>
         )}
-        {column && comparison && <input type="number" onChange={({ target }) => setValue(target.value)} />}
-        {column && comparison && value && <button type="button" onClick={() => onAddFilter()}>Filtrar</button>}
+        {column && comparison
+          && <input type="number" onChange={({ target }) => setValue(target.value)} />}
+        {column && comparison && value
+          && <button type="button" onClick={() => onAddFilter()}>Filtrar</button>}
       </div>
       <div>
-        {(filters[0].numericValues) && filters.map(({ numericValues }) => (
-          <div key={numericValues.column}>
-            <span>
-              {numericValues.column}
-              {' '}
-            </span>
-            <span>
-              {numericValues.comparison}
-              {' '}
-            </span>
-            <span>
-              {numericValues.value}
-              {' '}
-            </span>
-            <button
-              value={numericValues.column}
-              onClick={({ target }) => onExcludeFilter(target.value)}
-              type="button"
-            >
+        {(filters[0].numericValues) && filters.map(({ numericValues: num }) => (
+          <div key={num.column}>
+            <span>{`${num.column} `}</span>
+            <span>{`${num.comparison} `}</span>
+            <span>{`${num.value} `}</span>
+            <button value={num.column} onClick={(e) => onExcludeFilter(e.target.value)} type="button">
               X
             </button>
           </div>
         ))}
       </div>
-    </div >
+    </div>
   );
 };
 
