6b905a57edba686308cd01f245df9a8e937e047c
Andrey <andreytsuzuki@gmail.com>
2020-04-15 17:26:56 -0300

    fix

--- src/components/DropDownElement.js
+++ src/components/DropDownElement.js
@@ -4,16 +4,12 @@ import PropTypes from 'prop-types';
 function DropDownElement(props) {
   const { param, call } = props;
 
-  const clickHandle = (e) => {
-    call(e);
-  };
-
   return (
     <button
       key={param}
       type="button"
       name={param}
-      onClick={(e) => clickHandle(e)}
+      onClick={(e) => call(e)}
     >
       {param}
     </button>
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import DropDownElement from './DropDownElement';
 import './DropDown.css';
 
-
 function DropDown(props) {
   const {
     arr, func, name, index, testid,
