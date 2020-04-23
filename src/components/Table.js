532782d890b2ed64e3d47b99fe8832f59b09bd6d
lauro <laurolyra@gmail.com>
2020-04-23 13:19:36 -0300

    same movie. Same challenge

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,54 +1,29 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { SWContext } from '../context/starWarsContext';
 import './Table.css';
 
-const generateBody = (data, text) => (
-  data
-    .filter(({ name }) => name.toLowerCase().includes(text.toLowerCase()))
-    .map((values) => (
-      <tbody key={values.name}>
-        <tr>
-          {Object.values(values).map((box, index) => (index !== 9
-            ? <td className="tableData" data-testid={box} key={box}>{box}</td>
-            : null))}
-        </tr>
-      </tbody>
-    )));
-
-const generateTable = (loading, data, error, text) => {
-  if (!loading && data) {
-    return (
-      <table>
-        <thead>
-          <tr>
-            {Object.keys(data[0]).map((item) => (item !== 'residents'
-              ? <th className="tableHeader" key={item}>{item}</th>
-              : null))}
-          </tr>
-        </thead>
-        {generateBody(data, text)}
-      </table>
-    );
-  }
-  if (error) { return <div>{error}</div>; }
-  return <div>Loading...</div>;
-};
-
 const Table = () => {
-  const [text, setText] = useState('');
   const {
-    loading,
     data,
     error,
+    generateColumns,
+    generateComparison,
+    generateTable,
+    generateNumeric,
+    buttonFilters,
+    filters,
+    filterByText,
+    text,
   } = useContext(SWContext);
-  const filterByText = (string) => {
-    setText(string);
-  };
   return (
     <div>
       <input onChange={(e) => filterByText(e.target.value)} />
+      {generateColumns()}
+      {generateComparison()}
+      {generateNumeric()}
+      {buttonFilters()}
       <h2>Filters:</h2>
-      {generateTable(loading, data, error, text)}
+      {generateTable(data, error, text, filters)}
     </div>
   );
 };
