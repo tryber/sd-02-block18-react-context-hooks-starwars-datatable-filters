9ef93b39b344248b8f6f99727f697bdc21374e4c
guicgs <etohlif@protonmail.com>
2020-05-04 14:50:16 -0300

    corrigindo complexidade do provider

--- src/context/StarWarsContext.jsx
+++ src/context/StarWarsContext.jsx
@@ -3,26 +3,26 @@ import PropTypes from 'prop-types';
 
 export const StarWarsContext = createContext();
 
+const filterAll = (name, results, column, comparison, value) => {
+  const filtered = name
+    ? results.filter((planet) => planet.name.toLowerCase().match(name))
+    : results;
+  switch (comparison) {
+    case 'Maior que':
+      return filtered.filter((planet) => parseInt(planet[column], 10) > parseInt(value, 10));
+    case 'Menor que':
+      return filtered.filter((planet) => parseInt(planet[column], 10) < parseInt(value, 10));
+    case 'Igual a':
+      return filtered.filter((planet) => parseInt(planet[column], 10) === parseInt(value, 10));
+    default: return filtered;
+  }
+};
+
 export const StarWarsProvider = ({ children }) => {
   const [input, setInput] = useState('');
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [filters, setFilters] = useState([{ name: input }]);
-
-  const filterAll = (name, results, column, comparison, value) => {
-    const filtered = name
-      ? results.filter((planet) => planet.name.toLowerCase().match(name))
-      : results;
-    switch (comparison) {
-      case 'Maior que':
-        return filtered.filter((planet) => parseInt(planet[column], 10) > parseInt(value, 10));
-      case 'Menor que':
-        return filtered.filter((planet) => parseInt(planet[column], 10) < parseInt(value, 10));
-      case 'Igual a':
-        return filtered.filter((planet) => parseInt(planet[column], 10) === parseInt(value, 10));
-      default: return filtered;
-    }
-  };
 
   const filterByName = (name, results, allFilters) => {
     const [, ...rest] = allFilters;
