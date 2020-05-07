85db3f14587d1bf66b9b89fdb755829b166a5c7b
Mateus Talles <mateustalles@gmail.com>
2020-05-07 17:08:22 -0300

    created sort function

--- src/components/SortButton.js
+++ src/components/SortButton.js
@@ -7,32 +7,31 @@ export default function SortButton({ children, columnName }) {
     filters: [filters, setFilters],
   } = useContext(PlanetsDBContext);
 
-  const [sortOrder, setSortOrder] = useState('ASC');
+  const [sortOrder, setSortOrder] = useState('');
   const [isSorted, setIsSorted] = useState(false);
+
+  if (columnName === 'name') setSortOrder('ASC');
 
   const toggleSortOrder = () => {
     setIsSorted(true);
-    if (sortOrder === 'ASC') setSortOrder('DESC');
-    else if (sortOrder === '') setSortOrder('ASC');
-    else setSortOrder('ASC');
+    if (sortOrder === 'ASC') return setSortOrder('DESC');
+    if (sortOrder === '') return setSortOrder('ASC');
+    return setSortOrder('ASC');
   };
 
   useEffect(() => {
     function setSortings() {
-      filters.forEach((filter) => {
-        if (filter.column === columnName) {
-          return setFilters(filters.map(
-            (sortFilter) => {
-              if (sortFilter.column === columnName) return { ...sortFilter, order: sortOrder };
-              return sortFilter;
-            },
-          ));
-        }
-        return setFilters([...filters, { column: columnName, order: sortOrder }]);
-      });
+      return setFilters(filters.map(
+        (sortFilter) => {
+          if ('column' in sortFilter && sortFilter.column === columnName) {
+            return { ...sortFilter, order: sortOrder, isSorted };
+          }
+          return sortFilter
+            && setFilters([...filters, { column: columnName, order: sortOrder, isSorted }]);
+        },
+      ));
     }
     if (isSorted) setSortings();
-    setIsSorted(false);
   }, [filters, setFilters, isSorted, columnName, sortOrder]);
 
   return (<button type="button" onClick={toggleSortOrder}>{children}</button>);
