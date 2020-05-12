fa372c93265261c3806a3c8f8acd60a627ff5568
Mateus Talles <mateustalles@gmail.com>
2020-05-12 18:20:10 -0300

    Test Name Filtering 100%

--- src/tests/NameFilter.test.js
+++ src/tests/NameFilter.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cleanup } from '@testing-library/react';
+import { cleanup, fireEvent, wait } from '@testing-library/react';
 import renderWithRouter from '../services/renderWithRouter';
 import PlanetsDBProvider from '../context/PlanetsDBContext';
 import Table from '../components/Table';
@@ -16,5 +16,34 @@ describe('Tests Name Filter Input component', () => {
 
     const nameFilterInput = getByTestId('name-filter-input');
     expect(nameFilterInput).toBeInTheDocument();
+  });
+
+  it('filters planets by text input "an" properly', async () => {
+    const { getByTestId, getByText, queryByText } = renderWithRouter(
+      <PlanetsDBProvider>
+        <Table />
+      </PlanetsDBProvider>,
+    );
+
+    fireEvent.change(getByTestId('name-filter-input'), { target: { value: 'an' } });
+    await wait(() => getByText('Coruscant'));
+    expect(getByText('Alderaan')).toBeInTheDocument();
+    expect(getByText('Coruscant')).toBeInTheDocument();
+    expect(queryByText('Tatooine')).toBeNull();
+  });
+
+  it('filters planets by text input "t" properly', async () => {
+    const { getByTestId, getByText, queryByText } = renderWithRouter(
+      <PlanetsDBProvider>
+        <Table />
+      </PlanetsDBProvider>,
+    );
+
+    fireEvent.change(getByTestId('name-filter-input'), { target: { value: 't' } });
+    await wait(() => getByText('Tatooine'));
+    expect(getByText('Coruscant')).toBeInTheDocument();
+    expect(getByText('Hoth')).toBeInTheDocument();
+    expect(getByText('Tatooine')).toBeInTheDocument();
+    expect(queryByText('Alderaan')).toBeNull();
   });
 });
