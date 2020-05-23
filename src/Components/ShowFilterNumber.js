5ea079a361b24b184ace06c54c112ea2def4a887
richardwellerson <richardwellerson@gmail.com>
2020-05-23 11:05:17 -0300

    Code Climate

--- src/Components/ShowFilterNumber.js
+++ src/Components/ShowFilterNumber.js
@@ -1,6 +1,16 @@
 import React, { useContext } from 'react';
 import SwContext from '../Context';
 
+const removeButton = (filter, removeFilters) => (
+  <button
+    className="Return_Filter-button"
+    type="button"
+    onClick={() => removeFilters(filter)}
+  >
+    X
+  </button>
+);
+
 const ShowFilterNumber = () => {
   const {
     filters,
@@ -23,13 +33,7 @@ const ShowFilterNumber = () => {
           key={`show filter ${filter.column}`}
         >
           <p className="Return_Filter-text">{`${filter.column} | ${filter.comparison} | ${filter.value}`}</p>
-          <button
-            className="Return_Filter-button"
-            type="button"
-            onClick={() => removeFilters(filter)}
-          >
-            X
-          </button>
+          {removeButton(filter, removeFilters)}
         </div>
       ))}
     </div>
