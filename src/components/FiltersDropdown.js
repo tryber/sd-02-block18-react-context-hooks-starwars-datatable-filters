41f303f70e76f75489bc9e65780760d34aa8688c
Giorgia <giorgialeoniac@gmail.com>
2020-05-24 23:41:04 -0300

    Consertos do Code Climate, testes com cobertura ainda de 94.16%

--- src/components/FiltersDropdown.js
+++ src/components/FiltersDropdown.js
@@ -8,18 +8,59 @@ const todasAsColunas = [
   'rotation_period',
   'surface_water'];
 
-
-  const renderComparButton = (columnDrop, comparisonDrop, valueInput, adicionaFiltro) => (
-    <button
-      type="button"
-      className="filter-btn"
-      data-testid="botao-filtrar"
-      onClick={adicionaFiltro}
-      disabled={!(columnDrop && comparisonDrop && valueInput)}
+const renderColumnDrowpdown = (colunasRestantes, columnDrop, setColumn) => (
+  <div data-testid="filter">
+    <select
+      defaultValue=""
+      value={columnDrop}
+      onChange={(event) => setColumn(event.target.value)}
+      data-testid="column-inserted"
     >
+      <option value="" disabled>Selecione uma coluna</option>
+      {colunasRestantes.map((coluna) => <option key={coluna} value={coluna}>{coluna}</option>)}
+    </select>
+  </div>
+);
+
+const renderComparisonDropdown = (comparisonDrop, setComparison) => (
+  <div>
+    <select
+      defaultValue=""
+      value={comparisonDrop}
+      onChange={(event) => setComparison(event.target.value)}
+      data-testid="comparison-inserted"
+    >
+      <option value="" disabled>Selecione uma comparação</option>
+      <option value="Maior que">Maior que</option>
+      <option value="Menor que">Menor que</option>
+      <option value="Igual a">Igual a</option>
+    </select>
+  </div>
+);
+
+const renderNumberComparison = (valueInput, setValue) => (
+  <div>
+    <input
+      type="number"
+      value={valueInput}
+      placeholder="Digite um número"
+      data-testid="value-inserted"
+      onChange={(event) => setValue(event.target.value)}
+    />
+  </div>
+);
+
+const renderComparButton = (columnDrop, comparisonDrop, valueInput, adicionaFiltro) => (
+  <button
+    type="button"
+    className="filter-btn"
+    data-testid="botao-filtrar"
+    onClick={adicionaFiltro}
+    disabled={!(columnDrop && comparisonDrop && valueInput)}
+  >
         Filtrar
-    </button>
-  );
+  </button>
+);
 
 const FiltersDropdown = () => {
   const contextValues = useContext(StarWarsContext);
@@ -38,54 +79,17 @@ const FiltersDropdown = () => {
     setValue('');
   };
 
-  const renderColumnDrowpdown = (colunasRestantes, columnDrop) => (
-    <div data-testid="filter">
-      <select
-        defaultValue=""
-        value={columnDrop}
-        onChange={(event) => setColumn(event.target.value)}
-        data-testid="column-inserted"
-      >
-        <option value="" disabled>Selecione uma coluna</option>
-        {colunasRestantes.map((coluna) => <option key={coluna} value={coluna}>{coluna}</option>)}
-      </select>
-    </div>
-  );
-  const renderComparisonDropdown = (comparisonDrop) => (
-    <div>
-      <select
-        defaultValue=""
-        value={comparisonDrop}
-        onChange={(event) => setComparison(event.target.value)}
-        data-testid="comparison-inserted"
-      >
-        <option value="" disabled>Selecione uma comparação</option>
-        <option value="Maior que">Maior que</option>
-        <option value="Menor que">Menor que</option>
-        <option value="Igual a">Igual a</option>
-      </select>
-    </div>
-  );
-  const renderNumberComparison = (valueInput) => (
-    <div>
-      <input
-        type="number"
-        value={valueInput}
-        placeholder="Digite um número"
-        data-testid="value-inserted"
-        onChange={(event) => setValue(event.target.value)}
-      />
-    </div>
-  );
   const colunasRestantes = todasAsColunas.filter((coluna) => (
     !(arrayColunasJaSelecionadas.includes(coluna))
   ));
   return colunasRestantes.length !== 0 ? (
     <article>
-      {renderColumnDrowpdown(colunasRestantes, column)}
-      {renderComparisonDropdown(comparison)}
-      {renderNumberComparison(value)}
-      {renderComparButton(column, comparison, value, adicionaFiltro)}
+      <section className="comparisons">
+        {renderColumnDrowpdown(colunasRestantes, column, setColumn)}
+        {renderComparisonDropdown(comparison, setComparison)}
+        {renderNumberComparison(value, setValue)}
+        {renderComparButton(column, comparison, value, adicionaFiltro)}
+      </section>
     </article>
   ) : null;
 };
