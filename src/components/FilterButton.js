299cadb0ac4ee9ecf265c80b5b23c88b34689939
guicgs <etohlif@protonmail.com>
2020-05-26 20:58:54 -0300

    corrigido clear selects para chrome

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -2,18 +2,14 @@ import React, { useContext } from 'react';
 import { StarWarsContext } from '../context/StarWarsContext';
 
 const FilterButton = () => {
-  const { filters, numericFilter, setNumericFilter, setFilters } = useContext(StarWarsContext);
+  const { numericFilter } = useContext(StarWarsContext);
   const { column, comparison, value } = numericFilter;
   return (
     <input
-      type="reset"
+      type="submit"
       value="Filtrar"
       className={!(column && comparison && value) && 'filter-button-disabled'}
       data-testid="filter-button"
-      onClick={() => {
-        setFilters([...filters, { numericValues: { column, comparison, value } }]);
-        setNumericFilter({ column: '', comparison: '', value: '' });
-      }}
       disabled={!(column && comparison && value)}
     />
   );
